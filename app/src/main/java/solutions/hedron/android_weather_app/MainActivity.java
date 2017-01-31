e2a39c1fb73e5948ca6a4dfcae79fdd270a1af2a
Armin <armin.ag@outlook.com>
2017-01-31 19:38:31 +0100

    Implement Google Location Services with Permissions

--- app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
+++ app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
@@ -1,10 +1,26 @@
 package solutions.hedron.android_weather_app;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.Manifest;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSION_LOCATION = 111; // Some Arbitrary number
     private static MainActivity mainActivity;
 
     public static MainActivity getMainActivity() {
@@ -21,5 +37,61 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         MainActivity.setMainActivity(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this,this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    // Google Api Methods
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+        } else {
+            startLocationServices();
+        }
+    }
+
+    private void startLocationServices(){
+        try {
+            LocationRequest request = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+        } catch (SecurityException ex){
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSION_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startLocationServices();
+                } else {
+                    Toast.makeText(this, "Location can't be used if permission is denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
