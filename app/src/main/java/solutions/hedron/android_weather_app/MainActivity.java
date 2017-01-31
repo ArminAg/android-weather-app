833b23a5a2bad26ac3c9e25d31f3b9d4765d38b3
Armin <armin.ag@outlook.com>
2017-01-31 19:16:14 +0100

    Add Google Play Services

--- app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
+++ app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
@@ -3,7 +3,7 @@ package solutions.hedron.android_weather_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private static MainActivity mainActivity;
 
