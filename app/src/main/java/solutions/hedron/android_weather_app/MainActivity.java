499ff9bbfc7405e5f917c19657661541dd382593
Armin <armin.ag@outlook.com>
2017-02-01 20:15:44 +0100

    Add Temporary Api Code to MainActivity

--- app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
+++ app/src/main/java/solutions/hedron/android_weather_app/MainActivity.java
@@ -9,19 +9,55 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import solutions.hedron.android_weather_app.adapter.WeatherReportAdapter;
+import solutions.hedron.android_weather_app.model.DailyWeatherReport;
+import solutions.hedron.android_weather_app.service.DataService;
+import solutions.hedron.android_weather_app.service.WeatherApi;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     private GoogleApiClient mGoogleApiClient;
     private final int PERMISSION_LOCATION = 111; // Some Arbitrary number
     private static MainActivity mainActivity;
+
+    final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    final String URL_COORD = "/?lat=";//9.991354&lon=76.287129";
+    final String URL_UNITS = "&units=metric";
+    final String URL_API_KEY = "&APPID=60b31c8d0b09373fc658ed16b577cffa";
+
+    private TextView todayDate;
+    private TextView currentTemp;
+    private TextView lowTemp;
+    private ImageView weatherIcon;
+    private TextView cityCountry;
+    private TextView weatherDescription;
+
+    private ArrayList<DailyWeatherReport> weatherReportList = new ArrayList<>();
+    private WeatherReportAdapter adapter;
 
     public static MainActivity getMainActivity() {
         return mainActivity;
@@ -38,12 +74,99 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         MainActivity.setMainActivity(this);
 
+        this.todayDate = (TextView)findViewById(R.id.todayDate);
+        this.currentTemp = (TextView)findViewById(R.id.currentTemp);
+        this.lowTemp = (TextView)findViewById(R.id.lowTemp);
+        this.weatherIcon = (ImageView)findViewById(R.id.weatherIcon);
+        this.cityCountry = (TextView)findViewById(R.id.cityCountry);
+        this.weatherDescription = (TextView)findViewById(R.id.weatherDescription);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.content_daily_weather);
+        adapter = new WeatherReportAdapter(weatherReportList);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .enableAutoManage(this,this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+    }
+
+    private void updateUI(){
+        if (weatherReportList.size() > 0 ){
+            DailyWeatherReport report = weatherReportList.get(0);
+            todayDate.setText("Today, Jan 1");
+            currentTemp.setText(Integer.toString(report.getCurrentTemp()) + "°");
+            lowTemp.setText(Integer.toString(report.getMinTemp()) + "°");
+            cityCountry.setText(report.getCityName() + ", " + report.getCountry());
+            weatherDescription.setText(report.getWeather());
+
+            switch (report.getWeather()){
+                case DailyWeatherReport.WEATHER_TYPE_SUN:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.sunny));
+                case DailyWeatherReport.WEATHER_TYPE_CLOUDS:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.cloudy));
+                case DailyWeatherReport.WEATHER_TYPE_PARTIAL_CLOUDS:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.partially_cloudy));
+                case DailyWeatherReport.WEATHER_TYPE_RAIN:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.rainy));
+                case DailyWeatherReport.WEATHER_TYPE_SNOW:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.snow));
+                case DailyWeatherReport.WEATHER_TYPE_THUNDER:
+                    this.weatherIcon.setImageDrawable(ContextCompat.getDrawable(MainActivity.getMainActivity().getApplicationContext(), R.drawable.thunder_lightning));
+            }
+        }
+    }
+
+    public void downloadWeatherData(Location location){
+        final String fullcoords = URL_COORD + location.getLatitude() + "&lon=" + location.getLongitude();
+        final String url = URL_BASE + fullcoords + URL_UNITS + URL_API_KEY;
+
+        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject city = response.getJSONObject("city");
+                            String cityName = city.getString("name");
+                            String country = city.getString("country");
+
+                            JSONArray list = response.getJSONArray("list");
+
+                            for (int x = 0; x < 5; x++){
+                                JSONObject obj = list.getJSONObject(x);
+                                JSONObject main = obj.getJSONObject("main");
+                                Double currentTemp = main.getDouble("temp");
+                                Double maxTemp = main.getDouble("temp_max");
+                                Double minTemp = main.getDouble("temp_min");
+
+                                JSONArray weatherArr = obj.getJSONArray("weather");
+                                JSONObject weather = weatherArr.getJSONObject(0);
+                                String weatherType = weather.getString("main");
+
+                                String rawDate = obj.getString("dt_txt");
+
+                                DailyWeatherReport report = new DailyWeatherReport(cityName, country, currentTemp.intValue(),maxTemp.intValue(), minTemp.intValue(), weatherType, rawDate);
+                                weatherReportList.add(report);
+                            }
+                        } catch (JSONException exception){
+
+                        }
+                        updateUI();
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", "Error: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
     }
 
     // Google Api Methods
@@ -82,7 +205,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public void onLocationChanged(Location location) {
-
+        downloadWeatherData(location);
     }
 
     @Override
